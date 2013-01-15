5a8fa8a3c61f791cd26d7940d7adfd9981818408
Jonathan Sahm <contact@johnstyle.fr>
2013-01-15 08:17:07 +0100

    autoload classes + Nouvelles globales

--- phpTools.php
+++ phpTools.php
@@ -10,8 +10,17 @@
  * @link     https://github.com/johnstyle/phpTools
  */
 
-require 'class/str.class.php';
-require 'class/env.class.php';
-require 'class/tpl.class.php';
+spl_autoload_register(function($className) {
+    $path = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+    $path = str_replace('phpTools/', '', $path);
+    require 'class/' . $path . '.class.php';
+});
 
-defined('ROOT_TPL') OR define('ROOT_TPL', 'tpl/');
+defined('PHPTOOLS_CHARSET')  OR define('PHPTOOLS_CHARSET', 'UTF-8');
+defined('PHPTOOLS_ROOT_TPL') OR define('PHPTOOLS_ROOT_TPL', 'tpl/');
+
+defined('PHPTOOLS_DB_HOST')  OR define('PHPTOOLS_DB_HOST', 'localhost');
+defined('PHPTOOLS_DB_USER')  OR define('PHPTOOLS_DB_USER', 'root');
+defined('PHPTOOLS_DB_PWD')   OR define('PHPTOOLS_DB_PWD', '');
+defined('PHPTOOLS_DB_BASE')  OR define('PHPTOOLS_DB_BASE', '');
+
