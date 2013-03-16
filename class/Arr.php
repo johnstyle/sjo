4430dbb0744220a5ff93bc05730c4e51760c9477
Jonathan Sahm <contact@johnstyle.fr>
2013-03-16 15:36:56 +0100

    Mep method Arr::getTree()

--- class/Arr.php
+++ class/Arr.php
@@ -14,6 +14,23 @@ namespace phpTools;
 
 abstract class Arr
 {
+    public static function getTree($array, $tree, $value = false)
+    {
+        $tree = is_array($tree) && count($tree) == 1 ? $tree[0] : $tree;
+        if (is_array($tree)) {
+            $name = array_shift($tree);
+            if (isset($array[$name])) {
+                return self::getTree($array[$name], $tree, $value);
+            }
+        } else {
+            $name = $tree;
+            if (isset($array[$name])) {
+                return $array[$name];
+            }
+        }
+        return false;
+    }
+
     /**
      * Remplacement des espaces
      */
@@ -23,5 +40,16 @@ abstract class Arr
             return array_map('trim', explode($sep, $array));
         }
         return false;
+    }
+
+    /**
+     * Convertit une chaine en tableau
+     */
+    public static function to($array)
+    {
+        if (!is_array($array)) {
+            return array($array);
+        }
+        return $array;
     }
 }
