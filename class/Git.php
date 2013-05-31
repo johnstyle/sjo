585782c2af8ca642e09cb85f60cbfe64cd399923
Jonathan SAHM <jsahm@solire.fr>
2013-05-31 18:43:19 +0200

    Mep class Git

---
+++ class/Git.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * PHPTools
+ *
+ * PHP version 5
+ *
+ * @package  PHPTools
+ * @author   Jonathan Sahm <contact@johnstyle.fr>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/johnstyle/PHPTools.git
+ */
+
+namespace PHPTools;
+
+abstract class Git
+{
+    private static $HEAD;
+
+    /**
+     * Récupère la branche active du projet courant
+     *
+     * @param string $module Nom du sous-module, par défaut le projet principal est sélectionné
+     * @return string Nom de la branche
+     */
+    public static function branch($module = false)
+    {
+        if (!isset(self::$HEAD[$module])) {
+            $file = ROOT . '/.git/' . ($module ? 'modules/' . $module . '/' : '') . 'HEAD';
+
+            /** Ancienne version de Git */
+            if (!file_exists($file)) {
+                $file = ROOT . '/' . ($module ? $module . '/' : '') . '.git/HEAD';
+            }
+
+            $HEAD = file_get_contents($file);
+            if (preg_match("#refs/heads/(.+)$#", $HEAD, $match)) {
+                self::$HEAD[$module] = trim($match[1]);
+            }
+        }
+        return self::$HEAD[$module];
+    }
+}
