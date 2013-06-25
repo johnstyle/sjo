c39b63f0486c4284f78ceb952e6ec28ccb58e12b
Jonathan SAHM <contact@johnstyle.fr>
2013-06-25 18:53:15 +0200

    Correction Mvc Controler

--- class/Mvc/Controler.php
+++ class/Mvc/Controler.php
@@ -4,17 +4,39 @@ namespace PHPTools\Mvc;
 
 class Controler
 {
-    public function __construct($controler = 'controler')
+    public function __construct ($controler = 'controler', $method = 'method')
     {
-        if (!defined('CONTROLER')) {
-            define('CONTROLER', \PHPTools\Env::get($controler, 'Home'));
-        }
+        defined('CONTROLER') OR define('CONTROLER', \PHPTools\Env::get($controler, 'Home'));
+        defined('METHOD') OR define('METHOD', \PHPTools\Env::get($method));
+
         if (CONTROLER) {
-            $className = 'Core\\' . CONTROLER;
-            if (class_exists($className) && method_exists($className, '__load')) {
-                $class = new $className();
-                $class->__load();
+            $className = 'Controler\\' . CONTROLER;
+            if (class_exists($className)) {
+
+                $class = new $className ();
+
+                if (method_exists($class, '__load')) {
+                    $class->__load();
+                }
+
                 View::setObject(CONTROLER, $class);
+
+                switch(\PHPTools\Env::get('content_type')) {
+                    case 'json' :
+                        header('Content-type:application/json; charset=' . PHPTOOLS_CHARSET);
+                        if (method_exists($className, 'json' . METHOD)) {
+                            call_user_func($className . '::json' . METHOD);
+                        }
+                        exit ;
+                        break;
+                    default :
+                        header('Content-type:text/html; charset=' . PHPTOOLS_CHARSET);
+                        if (method_exists($className, METHOD)) {
+                            call_user_func($className . '::' . METHOD);
+                        }                        
+                        break;
+                }
+
                 View::inc(CONTROLER);
             }
         }
