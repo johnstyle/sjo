df7804135d78e8825c42658e4a9f9362a017f216
Jonathan SAHM <contact@johnstyle.fr>
2013-09-06 18:34:55 +0200

    Correction controler avec arborescence

--- class/Mvc/Controler.php
+++ class/Mvc/Controler.php
@@ -6,11 +6,11 @@ class Controler
 {
     public function __construct ($controler = 'controler', $method = 'method')
     {
-        defined('CONTROLER') OR define('CONTROLER', \PHPTools\Env::get($controler, 'Home'));
+        defined('CONTROLER') OR define('CONTROLER', trim(\PHPTools\Env::get($controler, 'Home'), '/'));
         defined('METHOD') OR define('METHOD', \PHPTools\Env::get($method));
 
         if (CONTROLER) {
-            $className = 'Controler\\' . CONTROLER;
+            $className = 'Controler\\' . str_replace('/', '\\', CONTROLER);
             if (class_exists($className)) {
 
                 $class = new $className ();
