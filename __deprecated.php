9ebccbe9c1f690942faadcb00f232c881ad868fe
Jonathan SAHM <contact@johnstyle.fr>
2013-09-07 12:47:02 +0200

    Mep retro-compatibilité

---
+++ __deprecated.php
@@ -0,0 +1,9 @@
+<?php
+
+if(defined('PHPTOOLS_ROOT_TPL') && !defined('PHPTOOLS_ROOT_VIEWS')) {
+    define('PHPTOOLS_ROOT_VIEWS', PHPTOOLS_ROOT_TPL);
+}
+
+if(defined('CONTROLER') && !defined('CONTROLLER')) {
+    define('CONTROLLER', CONTROLER);
+}
