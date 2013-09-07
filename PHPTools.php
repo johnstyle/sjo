eae21e3e96f5fe9e9ac6826bfef2f52eeb35bbeb
Jonathan SAHM <contact@johnstyle.fr>
2013-09-07 12:45:24 +0200

    Correction spl_autoload_register()

--- PHPTools.php
+++ PHPTools.php
@@ -10,9 +10,12 @@
  * @link     https://github.com/johnstyle/phpTools
  */
 
- /** Configuration */
+/** Compatibilité anciennes méthodes */
+include realpath(__DIR__) . '/__deprecated.php';
+
+/** Configuration */
 defined('PHPTOOLS_CHARSET') OR define('PHPTOOLS_CHARSET', 'UTF-8');
-defined('PHPTOOLS_ROOT_TPL') OR define('PHPTOOLS_ROOT_TPL', 'tpl/');
+defined('PHPTOOLS_ROOT_VIEWS') OR define('PHPTOOLS_ROOT_VIEWS', 'Views');
 defined('PHPTOOLS_DB_HOST') OR define('PHPTOOLS_DB_HOST', 'localhost');
 defined('PHPTOOLS_DB_USER') OR define('PHPTOOLS_DB_USER', 'root');
 defined('PHPTOOLS_DB_PWD') OR define('PHPTOOLS_DB_PWD', '');
@@ -20,11 +23,11 @@ defined('PHPTOOLS_DB_BASE') OR define('PHPTOOLS_DB_BASE', '');
 
 /** Classes */
 spl_autoload_register(
-    function ($className) {
-        if (strstr($className, 'PHPTools\\')) {
-            $className = str_replace('PHPTools\\', '', $className);
-            $className = str_replace('\\', '/', $className);
-            require realpath(__DIR__) . '/class/' . $className . '.php';
+    function ($class) {
+        $filename = str_replace('\\', '/', $class) . '.php';
+        if (strstr($filename, 'PHPTools/')) {
+            $filename = str_replace('PHPTools/', '', $filename);
+            include realpath(__DIR__) . '/class/' . $filename;
         }
     }
 );
