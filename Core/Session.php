8623dfeab23128f8b02afddebc6f49bbe8589c3d
Jonathan SAHM <contact@johnstyle.fr>
2013-09-17 13:00:53 +0200

    Correction class Session

--- Core/Session.php
+++ Core/Session.php
@@ -14,65 +14,63 @@ namespace PHPTools;
 
 class Session
 {
-    public static function start()
+    public static function start ()
     {
         if (session_status() == PHP_SESSION_NONE) {
             session_start();
         }
     }
 
-    public function check()
+    public function check ()
     {
         self::start();
 
-        if(CONTROLLER != PHPTOOLS_CONTROLLER_AUTH) {
-            if(!$this->isActive()) {
-                if(Libraries\Env::get('token')) {
+        if (CONTROLLER != PHPTOOLS_CONTROLLER_AUTH) {
+            if (!$this->isActive()) {
+                if (Libraries\Env::get('token')) {
                     $this->isActive(Libraries\Env::get('token'));
                 } else {
                     http_response_code(401);
                     $this->redirect('./?' . PHPTOOLS_CONTROLLER_NAME . '=' . PHPTOOLS_CONTROLLER_AUTH . '&redirect=' . urlencode('/?' . Libraries\Env::server('QUERY_STRING')));
                 }
             }
-        } elseif($this->isActive() && !METHOD) {                    
+        } elseif ($this->isActive() && !METHOD) {
             $this->redirect();
         }
     }
 
-    public function signup($token)
+    public function signin ($token)
     {
         Libraries\Env::sessionSet('token', $token);
         $this->redirect();
     }
 
-    public function signout()
+    public function signout ($url = './')
     {
         if (ini_get('session.use_cookies')) {
             $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000,
-                $params['path'], $params['domain'],
-                $params['secure'], $params['httponly']
-            );
-        }        
+            setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+        }
         session_destroy();
-        $this->redirect();
-    }    
+        $this->redirect($url);
+    }
 
-    public function redirect($url = './')
+    public function redirect ($url = './')
     {
-        if(Libraries\Env::get('redirect')) {
+        if (Libraries\Env::get('redirect')) {
             header('Location:.' . Libraries\Env::get('redirect'));
         } else {
             header('Location:' . $url);
         }
-        exit;
-    }    
+        exit ;
+    }
 
-    public function isActive()
+    public function isActive ()
     {
-        if(Libraries\Env::session('token')) {
+        if (Libraries\Env::session('token')) {
             return true;
         }
         return false;
     }
+
 }
