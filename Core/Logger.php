44b49d05192cd2f71c68af50ceaa4e9b276ecccb
Jonathan SAHM <contact@johnstyle.fr>
2013-11-02 00:42:08 +0100

    Mep class Logger

---
+++ Core/Logger.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PHPTools;
+
+/**
+ * This Logger can be used to avoid conditional log calls
+ *
+ * Logging should always be optional, and if no logger is provided to your
+ * library creating a NullLogger instance to have something to throw logs at
+ * is a good way to avoid littering your code with `if ($this->logger) { }`
+ * blocks.
+ */
+class Logger extends Log\AbstractLogger
+{
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        file_put_contents(
+            PHPTOOLS_ROOT_LOG . '/app.' . $level . '.log',
+            date('Y-m-d H:i:s') . "\t" . self::interpolate($message, $context) . "\n",
+            FILE_APPEND | LOCK_EX
+        );
+    }
+
+    public static function interpolate($message, array $context = array())
+    {
+        $replace = array();
+        foreach ($context as $key => $val) {
+            $replace['{' . $key . '}'] = $val;
+        }
+
+        return strtr($message, $replace);
+    }
+}
