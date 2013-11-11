cc373273b56219f7c1e97f234c842048ea2d5b73
Jonathan SAHM <contact@johnstyle.fr>
2013-11-11 20:17:28 +0100

    Mep class gestion des objets

---
+++ Libraries/DataObject.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PHPTools\Libraries;
+
+abstract class DataObject
+{
+    public function __get($name)
+    {
+        if(isset($this->{$name})) {
+            return $this->{$name};
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        if(isset($this->{$name})) {
+            $this->{$name} = $value;
+        }
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->{$name});
+    }
+}
