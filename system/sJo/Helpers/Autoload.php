e8d97b2b01ae23d5ab4cb38d43ad0ddea865fa1c
Jonathan SAHM <contact@johnstyle.fr>
2013-12-20 08:21:24 +0100

    Mep Helpers

---
+++ system/sJo/Helpers/Autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace sJo\Helpers;
+
+function Autoload($path)
+{
+	spl_autoload_register(
+	    function ($className) use ($path) {
+	        $className = str_replace('\\', '/', $className);
+            $filename = $path . '/' . $className . '.php';
+            if(file_exists($filename)) {
+	           require $filename;
+            }
+	    }
+	);
+}
