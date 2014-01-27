2ff43e992de23049d1eb1a2a699d24f5723039e5
Jonathan SAHM <contact@johnstyle.fr>
2014-01-28 00:31:36 +0100

    Utilisation method Lib\File::append()

--- system/sJo/Core/Logger.php
+++ system/sJo/Core/Logger.php
@@ -25,13 +25,10 @@ class Logger extends AbstractLogger
      */
     public function log($level, $message, array $context = array())
     {
-        file_put_contents(
-            SJO_ROOT_LOG . '/app.' . $level . '.log',
-                date('Y-m-d H:i:s') . "\t" .
-                Lib\Env::server('REQUEST_URI') ."\t" .
-                self::interpolate($message, $context) . "\n",
-            FILE_APPEND | LOCK_EX
-        );
+        Lib\File::append(SJO_ROOT_LOG . '/app.' . $level . '.log',
+            date('Y-m-d H:i:s') . "\t" .
+            Lib\Env::server('REQUEST_URI') ."\t" .
+            self::interpolate($message, $context) . "\n");
     }
 
     public static function interpolate($message, array $context = array())
