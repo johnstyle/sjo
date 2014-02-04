e4a0993fdef35ca65adbe7bf6edb99a8671d4544
Jonathan SAHM <contact@johnstyle.fr>
2014-02-05 00:20:05 +0100

    Mep autoloader

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
-include dirname(realpath(__DIR__)) . '/src/sJo/Libraries/Arr.php';
+define('ROOT', dirname(realpath(__DIR__)));
+
+spl_autoload_register(function ($className) {
+    $className = str_replace('\\', '/', $className);
+    if(strstr($className, 'sJo/')) {
+        $filename = ROOT . '/src/' . $className . '.php';
+        if(file_exists($filename)) {
+            require $filename;
+        }
+    }
+});
