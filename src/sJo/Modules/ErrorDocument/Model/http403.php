0477ea72d27713b9c37a660a1849aef3971074fc
Jonathan SAHM <contact@johnstyle.fr>
2014-02-06 23:37:17 +0100

    Suppression ancien systeme de Model

--- src/sJo/Modules/ErrorDocument/Model/http403.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace sJo\Modules\ErrorDocument\Model;
-
-use sJo\Core;
-
-class http403 extends Core\Model
-{
-}
--- src/sJo/Modules/ErrorDocument/Model/http404.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace sJo\Modules\ErrorDocument\Model;
-
-use sJo\Core;
-
-class http404 extends Core\Model
-{
-}
