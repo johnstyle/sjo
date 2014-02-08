ca7b9b57ad7039439c3bd2d80330a5a996cca9d8
Jonathan SAHM <contact@johnstyle.fr>
2014-02-08 16:45:09 +0100

    Mep Helper Bootstrap

--- src/sJo/View/Helper/Framework/Bootstrap/nav.php
+++ src/sJo/View/Helper/Framework/Bootstrap/nav.php
@@ -1,5 +1,23 @@
-<ul class="nav navbar-nav navbar-right">
-    <?php foreach(self::$view->items as $item): ?>
-        <li><a href="#"><?php echo $item['title']; ?></a></li>
-    <?php endforeach; ?>
-</ul>
+<<?php echo self::$view->container; ?>>
+    <ul class="nav <?php echo self::$view->container == 'aside' ? 'nav-sidebar' : 'navbar-nav'; ?><?php if(isset(self::$view->pull)): ?> navbar-<?php echo self::$view->pull; ?><?php endif; ?>">
+        <?php foreach(self::$view->items as $item): ?>
+            <li<?php if($item['controller'] == \sJo\Core\Loader::$controller): ?> class="active"<?php endif; ?>>
+                <a
+                    href="<?php echo $item['controller']; ?>"
+                    <?php if($item['tooltip']): ?>
+                        data-toggle="tooltip"
+                        <?php if($item['tooltip']): ?>
+                            data-placement="<?php $item['tooltip']['placement']; ?>"
+                        <?php endif; ?>
+                        title="<?php isset($item['tooltip']['title']) ? $item['tooltip']['title'] : $item['title']; ?>"
+                    <?php endif; ?>
+                    >
+                    <?php if($item['icon']): ?>
+                        <span class="glyphicon glyphicon-<?php echo $item['icon']; ?>>"></span>
+                    <?php endif; ?>
+                    <span class="title"><?php echo $item['title']; ?></span>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</<?php echo self::$view->container; ?>>
--- src/sJo/View/Helper/Framework/Framework.php
+++ src/sJo/View/Helper/Framework/Framework.php
@@ -1,17 +1,49 @@
 <?php
 
-namespace sJo\View\Framework;
+namespace sJo\View\Helper\Framework;
 
 class Framework
 {
     private static $view;
+    private static $frameworkName = 'Bootstrap';
+
+    public static function set($frameworkName = null)
+    {
+        if($frameworkName) {
+            self::$frameworkName = $frameworkName;
+        }
+    }
 
     public static function nav($items)
     {
-        self::$view = (object) array(
-            'items' => $items
-        );
+        return self::inc('nav.php', array(
+            'items' => $items,
+            'container' => 'nav',
+            'pull' => 'right'
+        ));
+    }
 
-        return include 'View/nav.php' ;
+    public static function aside($items)
+    {
+        return self::inc('nav.php', array(
+            'items' => $items,
+            'container' => 'aside'
+        ));
+    }
+
+    private static function inc()
+    {
+        $args = func_get_args();
+        $file = array_shift($args);
+
+        self::$view = new \stdClass();
+
+        foreach($args as $arg) {
+            foreach($arg as $name=>$value) {
+                self::$view->{$name} = $value;
+            }
+        }
+
+        return include self::$frameworkName . '/' . $file;
     }
 }
