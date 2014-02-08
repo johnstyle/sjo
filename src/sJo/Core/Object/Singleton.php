c599c04a19088827414d67c40c8c032f65354705
Jonathan SAHM <contact@johnstyle.fr>
2014-02-08 01:21:07 +0100

    Mep trait Object\Singleton

---
+++ src/sJo/Core/Object/Singleton.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace sJo\Core\Object;
+
+trait Singleton
+{
+    protected static $__instance;
+
+    public static function getInstance($args = null, $hash = null)
+    {
+        return isset(static::$__instance[$hash])
+            ? static::$__instance[$hash]
+            : static::$__instance[$hash] = new static($args);
+    }
+}
