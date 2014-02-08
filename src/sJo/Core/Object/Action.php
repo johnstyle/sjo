5a7c6be5a725702f892ab6a1984a59c90968f4ea
Jonathan SAHM <contact@johnstyle.fr>
2014-02-08 01:20:47 +0100

    Mep trait Object\Action

---
+++ src/sJo/Core/Object/Action.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace sJo\Core\Object;
+
+trait Action
+{
+    public function __construct()
+    {
+        if($this->getPrimaryValue() !== null) {
+
+            $result = self::db()->result(array(
+                $this->getPrimaryKey() => $this->getPrimaryValue()
+            ));
+
+            if($result) {
+                foreach($result as $key => $value) {
+                    $this->{$key} = $value;
+                }
+            }
+        }
+    }
+
+    public function delete()
+    {
+        if($this->getPrimaryValue()) {
+            return self::db()->delete(array(
+                $this->getPrimaryKey() => $this->getPrimaryValue()
+            ));
+        }
+
+        return false;
+    }
+
+    public function save()
+    {
+        $where = null;
+        if($this->getPrimaryValue()) {
+            $where = array(
+                $this->getPrimaryKey() => $this->getPrimaryValue()
+            );
+        }
+
+        return self::db()->update((array) $this->getProperties(), $where);
+    }
+}
