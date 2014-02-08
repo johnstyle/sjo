1c962081a5eeb0e3abc1eede51f5cce98f4a5c23
Jonathan SAHM <contact@johnstyle.fr>
2014-02-08 01:20:56 +0100

    Mep trait Object\Map

---
+++ src/sJo/Core/Object/Map.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace sJo\Core\Object;
+
+trait Map
+{
+    private $primaryKey;
+    private $tableName;
+
+    public function setPrimaryValue ($value)
+    {
+        $this->{$this->getPrimaryKey()} = $value;
+    }
+
+    public function getPrimaryValue ()
+    {
+        return $this->{$this->getPrimaryKey()};
+    }
+
+    public function getPrimaryKey ()
+    {
+        if (!$this->primaryKey) {
+            if(isset($this->__map['columns'])) {
+                foreach($this->__map['columns'] as $key=>$map) {
+                    if(isset($map['primary']) && $map['primary'] === true) {
+                        $this->primaryKey = $key;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return $this->primaryKey;
+    }
+
+    public function getTableName ()
+    {
+        if (!$this->tableName) {
+            if(isset($this->__map['table'])) {
+                $this->tableName = $this->__map['table'];
+            }
+        }
+
+        return $this->tableName;
+    }
+}
