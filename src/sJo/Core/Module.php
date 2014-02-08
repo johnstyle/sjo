5481ab03000b6a0f4fce3dd80aaaba6fb87c5153
Jonathan SAHM <contact@johnstyle.fr>
2014-02-08 01:25:00 +0100

    Mep interface type

--- src/sJo/Core/Module.php
+++ src/sJo/Core/Module.php
@@ -40,12 +40,12 @@ trait Module
 
     private function _setModule()
     {
-        $controller = '\\Controller\\' . self::$controller;
+        $controller = '\\' . self::$interface . '\\Controller\\' . self::$controller;
         if(!class_exists($controller) && preg_match("#^(.+?)\\\\([^\\\\]+)$#", self::$controller, $match)) {
             if(in_array($match[1], $this->modules)) {
                 self::$module = $match[1];
-                self::$controllerClass = '\\sJo\\Modules\\' . self::$module . '\\Controller\\' . $match[2];
-                self::$viewFile = realpath(__DIR__) . '/' . SJO_ROOT . '/Modules/' . self::$module . '/View/' . str_replace('\\', '/', $match[2]) . '.php';
+                self::$controllerClass = '\\sJo\\Modules\\' . self::$module . '\\' . self::$interface . '\\Controller\\' . $match[2];
+                self::$viewFile = realpath(__DIR__) . '/' . SJO_ROOT . '/Modules/' . self::$module . '/' . self::$interface . '/View/' . str_replace('\\', '/', $match[2]) . '.php';
             }
         }
     }
