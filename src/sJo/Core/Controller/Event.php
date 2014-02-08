b697ba9e5fbc075e0315a68109ad579f236bd42c
Jonathan SAHM <contact@johnstyle.fr>
2014-02-08 01:20:13 +0100

    Mep trait Controller/Event

---
+++ src/sJo/Core/Controller/Event.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace sJo\Core\Controller;
+
+trait Event
+{
+
+    public function __viewPreload ()
+    {
+    }
+
+    public function __viewLoaded ()
+    {
+    }
+
+    public function __viewCompleted ()
+    {
+    }
+}
