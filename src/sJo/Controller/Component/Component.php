ad853504e6fce1c1e3e5b7e1a355604a36b5c64c
Jonathan SAHM <contact@johnstyle.fr>
2014-02-27 00:53:04 +0100

    Suppression component Alert

--- src/sJo/Controller/Component/Component.php
+++ src/sJo/Controller/Component/Component.php
@@ -11,15 +11,12 @@ class Component
 
     /** @var Session $session */
     public $session;
-    /** @var Alert $alert */
-    public $alert;
     /** @var Logger $logger */
     public $logger;
 
     public function __construct(Controller $controller = null)
     {
         $this->session = new Session($controller);
-        $this->alert = new Alert($controller);
         $this->logger = new Logger($controller);
     }
 }
