929f1b6304d9a3bff246ee2633d382e0c5b65f9e
Jonathan SAHM <contact@johnstyle.fr>
2014-02-26 08:24:25 +0100

    Evolution Helper Table

--- src/sJo/View/Helper/Table.php
+++ src/sJo/View/Helper/Table.php
@@ -2,6 +2,7 @@
 
 namespace sJo\View\Helper;
 
+use sJo\Loader\Router;
 use sJo\View\Helper\Dom\Dom;
 use sJo\Libraries as Lib;
 
@@ -9,12 +10,57 @@ class Table extends Dom
 {
     public function setElement($element)
     {
-        return Lib\Arr::extend(array(
+        $element = Lib\Arr::extend(array(
             'id' => null,
             'class' => null,
             'thead' => null,
             'tfoot' => null,
-            'tbody' => null
+            'tbody' => null,
+            'actions' => null,
+            'bulk' => null
         ), $element);
+
+        $instance = null;
+
+        if (is_object($element['tbody'])) {
+            $instance = $element['tbody'];
+            $element['tbody'] = $instance->db()->results();
+        }
+
+        if ($element['thead'] === null && is_array($element['tbody'])) {
+            $thead = array();
+            foreach($element['tbody'] as $line) {
+                foreach($line as $name=>$value) {
+                    if (!in_array($name, $thead)) {
+                        $thead[] = $name;
+                    }
+                }
+            }
+            $element['thead'] = $thead;
+        }
+
+        if ($element['actions']) {
+            if ($element['thead'] && is_array($element['thead'])) {
+                array_push($element['thead'], Lib\I18n::__('Actions'));
+            }
+            if ($element['tbody'] && is_array($element['tbody'])) {
+                foreach($element['tbody'] as &$line) {
+                    $line->actions = '';
+                    foreach($element['actions'] as $action=>$options) {
+                        if(!is_array($options)) {
+                            $action = $options;
+                            $options = array(
+                                'href' => Router::link(null, array('method' => 'edit', $instance->getPrimaryKey() => $line->{$instance->getPrimaryKey()}))
+                            );
+                        }
+                        $line->actions .= Link::create(array_merge(array(
+                            'icon' => $action
+                        ), $options))->html();
+                    }
+                }
+            }
+        }
+
+        return $element;
     }
 }
