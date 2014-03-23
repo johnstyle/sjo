49044e9c72336a2fd769f6531237feac4e06d3ca
Jonathan SAHM <contact@johnstyle.fr>
2014-03-23 15:10:13 +0100

    Suppression des Session et du Logger des Components

--- src/sJo/Controller/Component/Component.php
+++ src/sJo/Controller/Component/Component.php
@@ -9,14 +9,8 @@ class Component
 {
     use Singleton;
 
-    /** @var Session $session */
-    public $session;
-    /** @var Logger $logger */
-    public $logger;
-
     public function __construct(Controller $controller = null)
     {
-        $this->session = new Session($controller);
-        $this->logger = new Logger($controller);
+
     }
 }
