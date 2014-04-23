0e0b159d149bc65b5c8a9d40ee39a84d342ced96
Jonathan SAHM <contact@johnstyle.fr>
2014-04-23 23:19:29 +0200

    Correction validation des champs

--- src/sJo/Model/Control/Validate.php
+++ src/sJo/Model/Control/Validate.php
@@ -6,10 +6,16 @@ use sJo\Libraries\I18n;
 
 trait Validate
 {
-    protected function validate ()
+    /**
+     * Validate fields values
+     *
+     * @return bool
+     */
+    public function validate ()
     {
-        foreach ($this->getTableFields() as $name => $attr) {
+        foreach ($this->getTableColumns() as $name => $attr) {
 
+            // Get field label
             $label = null;
 
             if (method_exists($this, 'getFormFieldDefinition')) {
@@ -22,22 +28,25 @@ trait Validate
                 $label = $name;
             }
 
-            if(isset($attr['required'])
-                && $attr['required']
-                && !$this->{$name}) {
+            if (is_null($this->{$name})
+                && '' === $this->{$name}) {
 
-                if(isset($attr['default'])) {
+                // Check required
+                if(isset($attr['required'])) {
 
-                    $this->{$name} = $attr['default'];
+                    if(isset($attr['default'])) {
 
-                } else {
+                        $this->{$name} = $attr['default'];
 
-                    $this->setError($name, I18n::__('The field %s is required.', '<strong>' . $label . '</strong>'));
+                    } else {
+
+                        $this->setError($name, I18n::__('The field %s is required.', '<strong>' . $label . '</strong>'));
+                    }
                 }
-            }
 
-            if ($this->{$name}) {
+            } else {
 
+                // Check length
                 if (!\sJo\Data\Validate::is($attr['type'], $this->{$name}, $attr['length'], $attr['values'])) {
 
                     $this->setError($name, I18n::__(
@@ -46,6 +55,27 @@ trait Validate
                         '<strong>' . $attr['type'] . '</strong>',
                         '<strong>' . $attr['length'] . '</strong>'
                     ));
+                }
+
+                // Check unique
+                if(isset($attr['unique'])
+                    && true === $attr['unique']) {
+
+                    $exists = $this->db()->value($this->getPrimaryKey(), array(
+                        $this->getPrimaryKey() => array(
+                            'value' => $this->getPrimaryValue(),
+                            'operator' => '!='
+                        ),
+                        $name => $this->{$name}
+                    ));
+
+                    if ($exists) {
+
+                        $this->setError($name, I18n::__(
+                            'This %s already exists.',
+                            '<strong>' . $label . '</strong>'
+                        ));
+                    }
                 }
             }
         }
