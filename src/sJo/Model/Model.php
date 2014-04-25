3b05f9db06b4969a180eaa93879467f5a2852785
Jonathan SAHM <contact@johnstyle.fr>
2014-04-25 07:58:02 +0200

    Ajout des methodes secure() validate() getFieldLabel()

--- src/sJo/Model/Model.php
+++ src/sJo/Model/Model.php
@@ -5,28 +5,76 @@ namespace sJo\Model;
 use sJo\Model\Database\DatabaseInterface;
 use sJo\Object\Singleton;
 use sJo\Object\Entity;
-use sJo\Request\Request;
 use sJo\Model\Database\Map;
 
 abstract class Model implements DatabaseInterface
 {
     use Singleton;
     use Entity;
-    use Control\Validate;
     use Control\Format;
     use Control\Error;
+    use Control\Validate {
+        Control\Validate::validate as private validateControl;
+    }
     use Map {
-        Map::__construct as private __MapConstruct;
+        Map::__construct as private __constructMap;
     }
 
     public function __construct($id = null)
     {
-        if($id === null) {
+        if (null !== $id) {
 
-            $id = Request::env('REQUEST')->{$this->getPrimaryKey()}->val();
+            $this->setPrimaryValue($id);
         }
 
-        $this->setPrimaryValue($id);
-        $this->__MapConstruct();
+        $this->__constructMap();
+    }
+
+    public function secure (array $fields)
+    {
+        $success = true;
+
+        if (null !== $this->getPrimaryValue()) {
+
+            foreach ($fields as $name=>$value) {
+
+                if ($this->{$name} !== $value) {
+
+                    $success = false;
+                }
+            }
+        }
+
+        return $success;
+    }
+
+    public function validate ()
+    {
+        $this->validateControl();
+
+        if (method_exists($this, 'validateForm')) {
+
+            $this->validateForm();
+        }
+
+        return !$this->hasErrors();
+    }
+
+    protected function getFieldLabel ($name)
+    {
+        // Get field label
+        $label = null;
+
+        if (method_exists($this, 'getFormFieldDefinition')) {
+
+            $label = $this->getFormFieldDefinition($name, 'label');
+        }
+
+        if (is_null($label)) {
+
+            $label = $name;
+        }
+
+        return $label;
     }
 }
