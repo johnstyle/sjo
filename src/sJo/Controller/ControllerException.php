7add5efaea9c66944d4d0674e44ea9d217e4c2a0
Jonathan SAHM <contact@johnstyle.fr>
2014-05-27 22:01:45 +0200

    Create ControllerException Class

---
+++ src/sJo/Controller/ControllerException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PHP version 5
+ *
+ * @package  sJo
+ * @category Core
+ * @author   Jonathan Sahm <contact@johnstyle.fr>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/johnstyle/sjo.git
+ */
+
+namespace sJo\Controller;
+
+/**
+ * Class ControllerException
+ * @package sJo\Controller
+ */
+class ControllerException extends \Exception
+{
+
+}
